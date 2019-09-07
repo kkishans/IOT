1bad98af74e7affdacea14357fe706e3f36db01e
nnikunjt <38680557+nnikunjt@users.noreply.github.com>
2019-09-07 07:40:01 +0530

    Update iot.sql

--- database/iot.sql
+++ database/iot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2019 at 06:32 AM
+-- Generation Time: Sep 07, 2019 at 04:07 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.0.32
 
@@ -25,6 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `temp` double(8,2) NOT NULL,
+  `humidity` double(8,2) NOT NULL,
+  `email` varchar(110) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roomName` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`id`, `temp`, `humidity`, `email`, `roomName`, `created_at`, `updated_at`) VALUES
+(26, 50.00, 50.00, 'nikunj@gmail.com', 'kichen', '2019-09-06 14:09:34', NULL),
+(27, 10.00, 50.00, 'nikunj@gmail.com', 'kichen', '2019-09-06 14:09:53', NULL),
+(28, 20.00, 80.00, 'nikunj@gmail.com', 'kichen', '2019-09-06 14:10:13', NULL),
+(29, 10.00, 80.00, 'nikunj@gmail.com', 'kichen', '2019-09-06 16:29:32', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -40,7 +66,10 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1);
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_09_02_112139_channel', 1),
+(4, '2019_09_03_120930_data', 2),
+(5, '2019_09_03_121404_room', 2);
 
 -- --------------------------------------------------------
 
@@ -53,6 +82,31 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `roomId` int(10) UNSIGNED NOT NULL,
+  `room` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`roomId`, `room`, `email`) VALUES
+(1, 'room1', 'nikunj@gmail.com'),
+(2, 'kichen', 'nikunj@gmail.com'),
+(4, 'bed room', 'nikunj@gmail.com'),
+(5, 'test', 'kishan@gmail.com'),
+(6, 'test2', 'kishan@gmail.com'),
+(7, 'kichen', 'kishan@gmail.com'),
+(8, 'h1', 'nikunj@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -71,8 +125,25 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Nikunj', 'nikunj@gmail.com', '$2y$10$7YAuQRHrTNTJFGzigfXXieviWDyWVImDyrkj2YgZzaJc7yqZ1JYqe', 'RkKxglee4y1bn6G9IgsrPeEZ8gxe5tYqDxO8fhie60cVL3Pww1dEFFZEafs8', '2019-09-02 06:05:14', '2019-09-02 06:05:14'),
+(2, 'Kishan', 'kishan@gmail.com', '$2y$10$jl30MXbew3H8jnYLDrd2H.NrMbObuYZo/acwZO17jPN.wKhXO/8tS', 'MoRjnEJSYlHZ6dUPUIQnTx9cyDNALsuhy7FDlFvVFCbvlv5X2tBopcIeQonY', '2019-09-04 22:20:20', '2019-09-04 22:20:20'),
+(3, 'Yash', 'yddc902@gmail.com', '$2y$10$PWS4bTf8YSbmtM54lo5oB.NbgWhofoH3ukJG24Le6Fjzbv7oA/1xi', NULL, '2019-09-05 22:47:17', '2019-09-05 22:47:17');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `roomId` (`roomName`),
+  ADD KEY `userId` (`email`);
 
 --
 -- Indexes for table `migrations`
@@ -87,6 +158,13 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`roomId`),
+  ADD KEY `email` (`email`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -98,16 +176,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `roomId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `data`
+--
+ALTER TABLE `data`
+  ADD CONSTRAINT `data_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
+
+--
+-- Constraints for table `room`
+--
+ALTER TABLE `room`
+  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
